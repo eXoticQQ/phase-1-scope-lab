7754fb05bda57592990246295755f59e4d316a04
Vitalii <vetal.shtager@gmail.com>
2022-04-13 08:38:39 -0400

    commit tree 0321af93e63f63d217933a6a90aacdd1543954cd

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+console.log("test 2:" + customerName)
+
+var bestCustomer
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+console.log("test 3:" + bestCustomer)
+
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+console.log("test 4:" + bestCustomer)
+const leastFavoriteCustomer = "not bob"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob for sure"
+    return leastFavoriteCustomer
+}
+
+console.log("test 5:" + leastFavoriteCustomer)
